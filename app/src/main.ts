3d295b9e7cf16a43854b9490b0f0b528f513a4bd
da.beloglazov <da.beloglazov@tensor.ru>
2024-11-10 19:13:35 +0300

    chore: test lib components

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,17 +1,15 @@
-import './assets/main.css'
 import '@packages/ui/styles'
+import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
 import { createVuetify } from '@packages/ui'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(createVuetify)
-app.use(router)
 
 app.mount('#app')
